18d7062006e4cde7b1c4d2abc46da2025c8f30ee
omar <omaralawii26@gmail.com>
2020-02-28 19:59:37 +0200

    Total-now-is-live

--- js/app.js
+++ js/app.js
@@ -8,11 +8,13 @@ var Seattle = {
     min: 23,
     max: 65,
     avg: 6.3,
+    totalCookiesPerDay : 0,
     randomCust: [],
     getCokies: function () {
-        for (let index = 0; index <Hour.length; index++) {
-             this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
-        };    
+        for (var index = 0; index < Hour.length; index++) {
+             this.randomCust.push(Math.floor(getRandomIntsalamon(this.min, this.max) * this.avg));
+            this.totalCookiesPerDay += this.randomCust[index];
+        }; 
         },
         Render : function () {
             var container = document.getElementById("sillsfile");
@@ -30,132 +32,157 @@ var Seattle = {
             for (let index = 0; index <Hour.length; index++){
                 var liEl = document.createElement('li');
                 liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
-                articleEl.appendChild(liEl);            }                 
+                articleEl.appendChild(liEl);}   
+                var totalel = document.createElement('li');
+                totalel.textContent =  `total ${this.totalCookiesPerDay}`;
+                liEl.appendChild(totalel);
+                         
             }
+          
 };
 Seattle.getCokies();
 Seattle.Render();
-// var Tokyo = {
-//     min: 3,
-//     max: 24,
-//     avg: 1.2,
-//     randomCust: [],
-//     getCokies: function () {
-//         for (let index = 0; index <Hour.length; index++) {
-//              this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
-//         };    
-//         },
-//         Render : function () {
-//             var container = document.getElementById("sillsfile");
-//             var articleEl = document.createElement('article');
-//             container.appendChild(articleEl);
+var Tokyo = {
+    min: 3,
+    max: 24,
+    avg: 1.2,
+    totalCookiesPerDay : 0,
+    randomCust: [],
+    getCokies: function () {
+        for (let index = 0; index <Hour.length; index++) {
+             this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
+           this.totalCookiesPerDay += this.randomCust[index];
+        };    
+        },
+        Render : function () {
+            var container = document.getElementById("sillsfile");
+            var articleEl = document.createElement('article');
+            container.appendChild(articleEl);
 
-//             var h2El = document.createElement('h2');
-//             h2El.textContent ='Tokyo';
-//             articleEl.appendChild(h2El);
-
-
-//             var ulEl = document.createElement('ul');
-//             articleEl.appendChild(ulEl); 
-
-//             for (let index = 0; index <Hour.length; index++){
-//                 var liEl = document.createElement('li');
-//                 liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
-//                 articleEl.appendChild(liEl);            }                 
-//             }
-// };
-// Tokyo.getCokies();
-// Tokyo.Render();
-// var Dubai = {
-//     min: 11,
-//     max: 38,
-//     avg: 3.7,
-//     randomCust: [],
-//     getCokies: function () {
-//         for (let index = 0; index <Hour.length; index++) {
-//              this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
-//         };    
-//         },
-//         Render : function () {
-//             var container = document.getElementById("sillsfile");
-//             var articleEl = document.createElement('article');
-//             container.appendChild(articleEl);
-
-//             var h2El = document.createElement('h2');
-//             h2El.textContent ='Dubai';
-//             articleEl.appendChild(h2El);
+            var h2El = document.createElement('h2');
+            h2El.textContent ='Tokyo';
+            articleEl.appendChild(h2El);
 
 
-//             var ulEl = document.createElement('ul');
-//             articleEl.appendChild(ulEl); 
+            var ulEl = document.createElement('ul');
+            articleEl.appendChild(ulEl); 
 
-//             for (let index = 0; index <Hour.length; index++){
-//                 var liEl = document.createElement('li');
-//                 liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
-//                 articleEl.appendChild(liEl);            }                 
-//             }
-// };
-// Dubai.getCokies();
-// Dubai.Render();
-// var Paris = {
-//     min: 20,
-//     max: 38,
-//     avg: 2.3,
-//     randomCust: [],
-//     getCokies: function () {
-//         for (let index = 0; index <Hour.length; index++) {
-//              this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
-//         };    
-//         },
-//         Render : function () {
-//             var container = document.getElementById("sillsfile");
-//             var articleEl = document.createElement('article');
-//             container.appendChild(articleEl);
+            for (let index = 0; index <Hour.length; index++){
+                var liEl = document.createElement('li');
+                liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
+                articleEl.appendChild(liEl);}  
+                var totalel = document.createElement('li');
+                 totalel.textContent =  `total ${this.totalCookiesPerDay}`;
+                 liEl.appendChild(totalel);               
+            }
+};
+Tokyo.getCokies();
+Tokyo.Render();
+var Dubai = {
+    min: 11,
+    max: 38,
+    avg: 3.7,
+    totalCookiesPerDay : 0,
+    randomCust: [],
+    getCokies: function () {
+        for (let index = 0; index <Hour.length; index++) {
+             this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
+             this.totalCookiesPerDay += this.randomCust[index];
+        };    
+        },
+        Render : function () {
+            var container = document.getElementById("sillsfile");
+            var articleEl = document.createElement('article');
+            container.appendChild(articleEl);
 
-//             var h2El = document.createElement('h2');
-//             h2El.textContent ='Paris';
-//             articleEl.appendChild(h2El);
-
-
-//             var ulEl = document.createElement('ul');
-//             articleEl.appendChild(ulEl); 
-
-//             for (let index = 0; index <Hour.length; index++){
-//                 var liEl = document.createElement('li');
-//                 liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
-//                 articleEl.appendChild(liEl);            }                 
-//             }
-// };
-// Paris.getCokies();
-// Paris.Render();
-// var Lima = {
-//     min: 2,
-//     max: 16,
-//     avg: 4.6,
-//     randomCust: [],
-//     getCokies: function () {
-//         for (let index = 0; index <Hour.length; index++) {
-//              this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
-//         };    
-//         },
-//         Render : function () {
-//             var container = document.getElementById("sillsfile");
-//             var articleEl = document.createElement('article');
-//             container.appendChild(articleEl);
-
-//             var h2El = document.createElement('h2');
-//             h2El.textContent ='Lima';
-//             articleEl.appendChild(h2El);
+            var h2El = document.createElement('h2');
+            h2El.textContent ='Dubai';
+            articleEl.appendChild(h2El);
 
 
-//             var ulEl = document.createElement('ul');
-//             articleEl.appendChild(ulEl); 
+            var ulEl = document.createElement('ul');
+            articleEl.appendChild(ulEl); 
 
-//             for (let index = 0; index <Hour.length; index++){
-//                 var liEl = document.createElement('li');
-//                 liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
-//                 articleEl.appendChild(liEl);            }                 
-//             }
-// };
-// Lima.getCokies();
-// Lima.Render();
+            for (let index = 0; index <Hour.length; index++){
+                var liEl = document.createElement('li');
+                liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
+                articleEl.appendChild(liEl);}
+                 var totalel = document.createElement('li');
+                 totalel.textContent =  `total ${this.totalCookiesPerDay}`;
+                 liEl.appendChild(totalel);                 
+            }
+};
+Dubai.getCokies();
+Dubai.Render();
+var Paris = {
+    min: 20,
+    max: 38,
+    avg: 2.3, 
+    totalCookiesPerDay : 0,
+    randomCust: [],
+    getCokies: function () {
+        for (let index = 0; index <Hour.length; index++) {
+             this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
+           this.totalCookiesPerDay += this.randomCust[index];
+        };    
+        },
+        Render : function () {
+            var container = document.getElementById("sillsfile");
+            var articleEl = document.createElement('article');
+            container.appendChild(articleEl);
+
+            var h2El = document.createElement('h2');
+            h2El.textContent ='Paris';
+            articleEl.appendChild(h2El);
+
+
+            var ulEl = document.createElement('ul');
+            articleEl.appendChild(ulEl); 
+
+            for (let index = 0; index <Hour.length; index++){
+                var liEl = document.createElement('li');
+                liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
+                articleEl.appendChild(liEl);} 
+                var totalel = document.createElement('li');
+                totalel.textContent =  `total ${this.totalCookiesPerDay}`;
+                liEl.appendChild(totalel);                
+            }
+};
+Paris.getCokies();
+Paris.Render();
+var Lima = {
+    min: 2,
+    max: 16,
+    avg: 4.6, 
+    totalCookiesPerDay : 0,
+    randomCust: [],
+    getCokies: function () {
+        for (let index = 0; index <Hour.length; index++) {
+             this.randomCust.push(Math.floor(((getRandomIntsalamon(this.min, this.max) * this.avg))));
+         this.totalCookiesPerDay += this.randomCust[index];
+        };    
+        },
+        Render : function () {
+            var container = document.getElementById("sillsfile");
+            var articleEl = document.createElement('article');
+            container.appendChild(articleEl);
+
+            var h2El = document.createElement('h2');
+            h2El.textContent ='Lima';
+            articleEl.appendChild(h2El);
+
+
+            var ulEl = document.createElement('ul');
+            articleEl.appendChild(ulEl); 
+
+            for (let index = 0; index <Hour.length; index++){
+                var liEl = document.createElement('li');
+                liEl.textContent = `${Hour[index] }: ${this.randomCust[index]}` ;
+                articleEl.appendChild(liEl);}    
+                var totalel = document.createElement('li');
+                totalel.textContent =  `total ${this.totalCookiesPerDay}`;
+                liEl.appendChild(totalel);              
+            }
+};
+Lima.getCokies();
+Lima.Render();
